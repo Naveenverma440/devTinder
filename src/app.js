981278497f011014f88278ee0707230803852d3b
NAVEEN KUMAR <naveenverma440@gmail.com>
2025-03-10 09:42:29 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 
 const app = express();
-app.use("/hello",(req, res) => {
+// app.use("/",(req, res) => {
+//   res.send('naveen'); 
+// })
+// app.use("/hello",(req, res) => {
+//     res.send('hello naveen'); 
+//   })
+
+  app.use("/test",(req, res) => {
     res.send('hello naveen'); 
   })
-app.use((req, res) => {
-  res.send('hello world'); 
+// app.use((req, res) => {
+//   res.send('hello world'); 
+// })
+
+app.get('/user', (req, res) => {
+  res.send({"name":"naveen","age":23});
 })
 
-
+app.post('/user', (req, res) => {
+  res.send("data saved");
+})
 
 app.listen(3001,()=>{
     console.log('listening on port 3001');
